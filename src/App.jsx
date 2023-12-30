c788d30bf5ff3a62ca055124fdc05926f57bf2e9
raushanraj9 <raushanr363@gmail.com>
2023-12-30 21:04:34 +0530

    set up all the files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-zinc-700 font-bold text-lg'>TodoList</h1>
+
+    </>
+  )
+}
+
+export default App
